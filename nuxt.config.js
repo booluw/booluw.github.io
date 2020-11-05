366046f2385c1b370eceb604f5e2eb72745fef73
Boluwatife Johnson <jaycewap@gmail.com>
2020-11-05 17:29:14 +0100

    Added project fonts.

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'assets/scss/main.css',
+    'assets/font/ionicons.min.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
