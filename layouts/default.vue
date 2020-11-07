2cafb515af55048f46f3c1fb42fb7d5fa2f8e555
Boluwatife Johnson <jaycewap@gmail.com>
2020-11-07 19:01:15 +0100

    Done with header.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
+    <header class="header">
+      <input type="checkbox" id="menu-toggler" />
+      <div class="header__logo">booluw.me</div>
+      <label for="menu-toggler" class="header__hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+      </label>
+      <nav class="header__nav">
+        <div class="nav-header">
+          <p class="nav-header__text">
+            <span>&larr;</span> Menu <span>&rarr;</span>
+          </p>
+          <label for="menu-toggler" class="header__hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+          </label>
+        </div>
+        <ul class="nav">
+          <li>
+            <a href="#projects" class="nav__link">
+              Projects <span>&rarr;</span>
+            </a>
+          </li>
+          <li>
+            <a href="#contact" class="nav__link">
+              Contact <span>&rarr;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">
-        Dress.me
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    Hello World
   </div>
 </template>
 
